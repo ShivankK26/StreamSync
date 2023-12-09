b177e7ce44c6522de95534727dc842b170605736
Shivank Kapur <shivankkapur2004@gmail.com>
2023-12-10 03:00:46 +0530

    custom api errors

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser' // used to perform CRUD operations in the cookies of user's browser
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // used in middlewares, for cross platform efficient working
+    credentials: true
+})) 
+
+app.use(express.json({ limit: "16kb" })) // it controls the limit of the json data being received so that the server doesn't crash
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // it basically reads the entire url of a webpage even if there is any other sign like % between two characters.
+app.use(express.static('public')) // a middleware used for accessing all the data in public folder
+app.use(cookieParser())
+
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
 dotenv.config({path: './env'})
+import express from 'express'
+
+const app = express()
 
 
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB Connection Failed.", err);
+})
 
 
 
