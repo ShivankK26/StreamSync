8117f591cad4488d7c0e90ed9d2f8560934e5cc2
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2023-12-23 21:38:02 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -284,7 +284,7 @@ export const changeCurrentPassword = asyncHandler(async(req, res) => {
 export const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 
